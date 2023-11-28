c1995fc3026ab97ce41bd93af28a30f3102a66de
ankita-kumari1031 <ankitakumari3110@gmail.com>
2023-11-28 17:33:43 +0530

    commit.

---
+++ src/app/features/category/services/category.service.ts
@@ -0,0 +1,35 @@
+import { UpdateCategoryRequest } from './../models/update-category-request.model';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AddCategoryRequest } from '../models/add-category-request.model';
+import { Category } from '../models/categoryModel';
+import { environment } from '../../../../environments/environment.development';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  constructor(private http: HttpClient) {}
+
+  addCategory(model: AddCategoryRequest): Observable<void> {
+    return this.http.post<void>(`${environment.apiBaseUrl}/api/Categories`, model);
+  }
+
+  getAllCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(`${environment.apiBaseUrl}/api/Categories`);
+  }
+
+  getCategoryById(id: string): Observable<Category> {
+    return this.http.get<Category>(`${environment.apiBaseUrl}/api/Categories/${id}`);
+  }
+
+  updateCategory(id: string, updateCategoryRequest: UpdateCategoryRequest): Observable<Category> {
+    return this.http.put<Category>(`${environment.apiBaseUrl}/api/categories/${id}`, updateCategoryRequest);
+  }
+
+  deleteCategory(id: string): Observable<Category> {
+    return this.http.delete<Category>(`${environment.apiBaseUrl}/api/Categories/${id}`);
+  }
+}
